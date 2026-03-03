1bd8c83dcf599e4a83852bba591d01630d9548f6
Tiberius Gherac <tiberius.gherac@gmail.com>
2026-03-03 21:31:11 +0100

    Add error handling to bootstrap function in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(process.env.PORT!);
+    console.log(`Server running on port ${process.env.PORT}`);
+  } catch (err) {
+    console.error(err);
+    process.exitCode = 1;
+  }
 }
+
 bootstrap();
