c08dc3da2c904ae2c766b08003e0d14fa9e7561f
Tiberius Gherac <tiberius.gherac@gmail.com>
2026-03-06 13:14:00 +0100

    Add prescription working draft

--- src/prescriptions/prescription.model.ts
+++ src/prescriptions/prescription.model.ts
@@ -3,9 +3,6 @@ import { Field, Float, Int, ObjectType } from '@nestjs/graphql';
 @ObjectType()
 export class Prescription {
   @Field(() => Int)
-  id: number;
-
-  @Field(() => Int)
   year: number;
 
   @Field()
@@ -15,13 +12,13 @@ export class Prescription {
   atcCode: string;
 
   @Field()
-  sex: string;
+  gender: string;
 
   @Field()
   ageGroup: string;
 
   @Field(() => Int)
-  numberOfDispensings: number;
+  numberOfPrescriptions: number;
 
   @Field(() => Int)
   numberOfPatients: number;
--- src/prescriptions/prescriptions.module.ts
+++ src/prescriptions/prescriptions.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../database/database.module';
 import { PrescriptionsResolver } from './prescriptions.resolver';
 import { PrescriptionsService } from './prescriptions.service';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [PrescriptionsResolver, PrescriptionsService],
 })
 export class PrescriptionsModule {}
