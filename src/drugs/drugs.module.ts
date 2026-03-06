c3bd5db4c5092e79aded3bc53f445c97594faab2
Tiberius Gherac <tiberius.gherac@gmail.com>
2026-03-06 14:03:08 +0100

    Refactor Drug model and service to work with ETL format and sqldb

--- src/drugs/drugs.module.ts
+++ src/drugs/drugs.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../database/database.module';
 import { DrugsResolver } from './drugs.resolver';
 import { DrugsService } from './drugs.service';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [DrugsResolver, DrugsService],
 })
 export class DrugsModule {}
--- src/drugs/drugs.resolver.ts
+++ src/drugs/drugs.resolver.ts
@@ -7,12 +7,12 @@ export class DrugsResolver {
   constructor(private readonly drugsService: DrugsService) {}
 
   @Query(() => [Drug])
-  drugs(): Drug[] {
+  async drugs(): Promise<Drug[]> {
     return this.drugsService.findAll();
   }
 
   @Query(() => Drug, { nullable: true })
-  drug(@Args('atcCode') atcCode: string): Drug | undefined {
+  async drug(@Args('atcCode') atcCode: string): Promise<Drug | undefined> {
     return this.drugsService.findOne(atcCode);
   }
 }
