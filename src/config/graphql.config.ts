87882a03e9ddc480ab3c0d8cbf71211542011932
Tiberius Gherac <tiberius.gherac@gmail.com>
2026-03-09 15:29:43 +0100

    Uppdate error handling in GraphQL configuration for better user feedback

--- src/config/graphql.config.ts
+++ src/config/graphql.config.ts
@@ -5,6 +5,10 @@ export const graphqlConfig: ApolloDriverConfig = {
   autoSchemaFile: true,
   formatError: (error) => {
     console.error(error);
+    const originalError = error.extensions?.originalError as any;
+    if (originalError?.statusCode === 400) {
+      return { message: originalError.message, extensions: { code: 'BAD_USER_INPUT' } };
+    }
     if (process.env.NODE_ENV !== 'dev') {
       return { message: 'Internal server error' };
     }
