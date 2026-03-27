0e3bafc5cce31e9051b89ed41c8a4ec793778c29
Tiberius Gherac <tiberius.gherac@gmail.com>
2026-03-27 08:40:02 +0100

    Set 'trust proxy' in order for the rate limiter to work as intended

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
+    app.getHttpAdapter().getInstance().set('trust proxy', 1);
     await app.listen(process.env.PORT!);
     console.log(`Server running on port ${process.env.PORT}`);
   } catch (err) {
